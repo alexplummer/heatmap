f7dd5231e259b3f47a039e678eaf3f410fb6efc0
Plummer <darkguile86@gmail.com>
2017-11-17 14:12:23 +0000

    Initial setup of React

---
+++ my-app/src/components/new/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './components/map/App.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Map;
